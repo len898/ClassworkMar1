79942754e81faec09c866d827753c649292f9f5f
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-08 13:41:41 -0800

    Finishd Lab, cleanup later and comment

--- ClassworkMar1/Source.cpp
+++ ClassworkMar1/Source.cpp
@@ -1,16 +1,27 @@
+/** Start by copying in your code from part I.
+ *  You need to finish part I before starting
+ *  part II.
+ **/
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
 
-#define DICT_FILE_NAME "English_words_lower_case -partial.txt"
-#define NEW_DICT_FILE "Updated Dictionary.txt"
+using namespace std;
 
+#define NEW_DICT_FILE_PREFIX "dict-"
+#define NEW_DICT_FILE_SUFFIX ".txt"
+
+std::vector<std::string>::iterator traverseVector(std::string userString, std::vector<std::string>& vectorOfStrings);
+std::vector<std::string>::iterator insertWord(std::string userString, std::vector<std::string>& vectorOfStrings);
+std::string getWordFromUser();
 
 int main() {
-	std::ifstream inputStream(DICT_FILE_NAME);
+	std::string userWord = getWordFromUser();
+	std::ifstream inputStream("dict.txt");
 	unsigned int i;
 	std::vector<std::string> vectorOfStrings;
+
 
 	if (inputStream.is_open()) {
 		std::string currentWord;
@@ -19,17 +30,32 @@ int main() {
 			vectorOfStrings.push_back(currentWord);
 		}
 	}
+	inputStream.close();
 
-	std::ifstream inputStreamForOutput(NEW_DICT_FILE);
+	std::vector<std::string>::iterator functionReturn;
+
+
+	functionReturn = traverseVector(userWord, vectorOfStrings);
+	if (functionReturn == vectorOfStrings.end()) {
+		std::cout << "Your word was \'" << userWord << "\'. We did not find your word. Adding word to dictionary... \n Inserted!";
+		insertWord(userWord, vectorOfStrings);
+	}
+	else {
+		std::vector<std::string>::iterator nextWord = functionReturn + 1;
+		std::cout << "Your word was \'" << userWord << "\'. The next word would be \'" << *nextWord << "\'.";
+	}
+
+
+
+	std::ifstream inputStreamForOutput(NEW_DICT_FILE_PREFIX + userWord + NEW_DICT_FILE_SUFFIX);
 	if (inputStreamForOutput.is_open()) {
 		return 1;
 	}
 
-	std::ofstream outputStream(NEW_DICT_FILE);
+	std::ofstream outputStream(NEW_DICT_FILE_PREFIX + userWord + NEW_DICT_FILE_SUFFIX);
 
 	if (outputStream.is_open()) {
 		for (i = 0; i < vectorOfStrings.size(); i++) {
-			std::cout << "HERE";
 			outputStream << vectorOfStrings.at(i) << std::endl;
 		}
 	}
@@ -37,4 +63,45 @@ int main() {
 
 
 	return 0;
+}
+
+std::vector<std::string>::iterator traverseVector(std::string userString, std::vector<std::string>& vectorOfWords)
+{
+	std::vector<std::string>::iterator myIterator;
+	myIterator = vectorOfWords.begin();
+	while (myIterator != vectorOfWords.end()) {
+		if (*myIterator == userString) {
+			return myIterator;
+		}
+		myIterator++;
+	}
+	return myIterator;
+
+}
+
+std::vector<std::string>::iterator insertWord(std::string userString, std::vector<std::string>& vectorOfStrings)
+{
+	std::vector<std::string>::iterator myIterator = vectorOfStrings.begin();
+	while (myIterator != vectorOfStrings.end()) {
+		std::vector<std::string>::iterator nextWord = myIterator + 1;
+		if (*nextWord >= userString && userString >= *myIterator) {
+			myIterator = vectorOfStrings.insert(nextWord, userString);
+			return nextWord;
+		}
+		myIterator++;
+	}
+	return myIterator;
+}
+
+std::string getWordFromUser()
+{
+	std::string userStr;
+	//std::cout << "Enter a word: ";
+	std::cin >> userStr;
+	while (userStr.length() <= 0) {
+		std::cout << "Enter a word: ";
+		std::cin >> userStr;
+	}
+	return userStr;
+
 }
